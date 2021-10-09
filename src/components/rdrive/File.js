ff8a26a3a4b19bb4d5c6c9a3471b79e145fb00a5
RichProsper <r.stp.p@hotmail.com>
2021-10-09 00:34:21 -0400

    Display icon based on file type (2)

--- src/components/rdrive/File.js
+++ src/components/rdrive/File.js
@@ -3,62 +3,56 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useState, useEffect } from 'react'
 
 const FILE_TYPES = {
-    WORD_1: 'application/msword',
-    WORD_2: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-    VIDEO_1: 'video/mp4',
-    VIDEO_2: 'video/x-matroska',
-    VIDEO_3: 'video/webm',
-    VIDEO_4: 'video/quicktime',
-    VIDEO_5: 'video/x-ms-wmv',
-    POWERPOINT_1: 'application/vnd.ms-powerpoint',
-    POWERPOINT_2: 'application/vnd.ms-powerpoint.presentation.macroEnabled.12',
-    POWERPOINT_3: 'application/vnd.openxmlformats-officedocument.presentationml.presentation',
-    PDF: 'application/pdf',
-    IMAGE_1: 'image/jpeg',
-    IMAGE_2: 'image/png',
-    IMAGE_3: 'image/webp',
-    EXCEL_1: 'application/vnd.ms-excel.sheet.binary.macroEnabled.12',
-    EXCEL_2: 'application/vnd.openxmlformats-officedocument.spreadsheetml.template',
-    EXCEL_3: 'application/vnd.ms-excel.sheet.macroEnabled.12',
-    EXCEL_4: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-    // CODE
-    // AUDIO
-    // ARCHIVE
-    // CSV
-    TEXT_PLAIN: 'text/plain'
+    VIDEO: 'video',
+    IMAGE: 'image',
+    AUDIO: 'audio',
+    TEXT: 'text',
+    TEXT_TYPES: {
+        PLAIN: 'plain',
+        CODE_1: 'html',
+        CODE_2: 'css',
+        CODE_3: 'javascript',
+    },
+    APPLICATION: 'application',
+    APPLICATION_TYPES: {
+        WORD_1: 'msword',
+        WORD_2: 'vnd.openxmlformats-officedocument.wordprocessingml.document',
+        POWERPOINT_1: 'vnd.ms-powerpoint',
+        POWERPOINT_2: 'vnd.ms-powerpoint.presentation.macroEnabled.12',
+        POWERPOINT_3: 'vnd.openxmlformats-officedocument.presentationml.presentation',
+        PDF: 'pdf',
+        EXCEL_1: 'vnd.ms-excel.sheet.binary.macroEnabled.12',
+        EXCEL_2: 'vnd.openxmlformats-officedocument.spreadsheetml.template',
+        EXCEL_3: 'vnd.ms-excel.sheet.macroEnabled.12',
+        EXCEL_4: 'vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        ARCHIVE_1: 'x-gzip',
+        ARCHIVE_2: 'x-tar',
+        ARCHIVE_3: 'x-zip-compressed',
+        CSV: 'vnd.ms-excel'
+    }
 }
 
 export default function File({ file }) {
     const [fileIcon, setFileIcon] = useState(null)
+
     const dArr = file.modifiedAt ? new Date(file.modifiedAt.toDate()).toDateString().split(' ') : null
     const d = dArr ? dArr[2] + ' ' + dArr[1] + ' ' + dArr[3] : ''
-    console.log(file.type)
 
     const formatSize = size => {
-        if (!size) return ''
+        if (size === null || size === undefined) return ''
         if (size >= 1024 * 1024) return `${(size / 1024 / 1024).toFixed(2)} MB`
         if (size >= 100 * 1024) return `${(size / 1024).toFixed(0)} KB`
         if (size >= 10 * 1024) return `${(size / 1024).toFixed(1)} KB`
         if (size >= 1024) return `${(size / 1024).toFixed(2)} KB`
-        return `${size} bytes`
+        if (size !== 1) return `${size} bytes`
+        return `${size} byte`
     }
 
     useEffect(() => {
-        switch (file.type) {
-            case FILE_TYPES.WORD_1 :
-            case FILE_TYPES.WORD_2 :
-                (async () => {
-                    const { faFileWord } = await import('@fortawesome/free-solid-svg-icons')
-                    return faFileWord
-                })().then(icon => setFileIcon(icon))
+        const type = file.type.split("/")
 
-               return
-
-            case FILE_TYPES.VIDEO_1 :
-            case FILE_TYPES.VIDEO_2 :
-            case FILE_TYPES.VIDEO_3 :
-            case FILE_TYPES.VIDEO_4 :
-            case FILE_TYPES.VIDEO_5 :
+        switch (type[0]) {
+            case FILE_TYPES.VIDEO :
                 (async () => {
                     const { faFileVideo } = await import('@fortawesome/free-solid-svg-icons')
                     return faFileVideo
@@ -66,27 +60,7 @@ export default function File({ file }) {
 
                return
 
-            case FILE_TYPES.POWERPOINT_1 :
-            case FILE_TYPES.POWERPOINT_2 :
-            case FILE_TYPES.POWERPOINT_3 :
-                (async () => {
-                    const { faFilePowerpoint } = await import('@fortawesome/free-solid-svg-icons')
-                    return faFilePowerpoint
-                })().then(icon => setFileIcon(icon))
-
-               return
-
-            case FILE_TYPES.PDF :
-                (async () => {
-                    const { faFilePdf } = await import('@fortawesome/free-solid-svg-icons')
-                    return faFilePdf
-                })().then(icon => setFileIcon(icon))
-
-               return
-
-            case FILE_TYPES.IMAGE_1 :
-            case FILE_TYPES.IMAGE_2 :
-            case FILE_TYPES.IMAGE_3 :
+            case FILE_TYPES.IMAGE :
                 (async () => {
                     const { faFileImage } = await import('@fortawesome/free-solid-svg-icons')
                     return faFileImage
@@ -94,24 +68,106 @@ export default function File({ file }) {
 
                 return
 
-            case FILE_TYPES.EXCEL_1 :
-            case FILE_TYPES.EXCEL_2 :
-            case FILE_TYPES.EXCEL_3 :
-            case FILE_TYPES.EXCEL_4 :
+            case FILE_TYPES.AUDIO :
                 (async () => {
-                    const { faFileExcel } = await import('@fortawesome/free-solid-svg-icons')
-                    return faFileExcel
+                    const { faFileAudio } = await import('@fortawesome/free-solid-svg-icons')
+                    return faFileAudio
                 })().then(icon => setFileIcon(icon))
 
                 return
 
-            case FILE_TYPES.TEXT_PLAIN :
-                (async () => {
-                    const { faFileAlt } = await import('@fortawesome/free-solid-svg-icons')
-                    return faFileAlt
-                })().then(icon => setFileIcon(icon))
+            
+            // Text Types
+            case FILE_TYPES.TEXT :
+                switch (type[1]) {
+                    case FILE_TYPES.TEXT_TYPES.PLAIN :
+                        (async () => {
+                            const { faFileAlt } = await import('@fortawesome/free-solid-svg-icons')
+                            return faFileAlt
+                        })().then(icon => setFileIcon(icon))
+        
+                       return
 
-               return
+                    case FILE_TYPES.TEXT_TYPES.CODE_1 :
+                    case FILE_TYPES.TEXT_TYPES.CODE_2 :
+                    case FILE_TYPES.TEXT_TYPES.CODE_3 :
+                        (async () => {
+                            const { faFileCode } = await import('@fortawesome/free-solid-svg-icons')
+                            return faFileCode
+                        })().then(icon => setFileIcon(icon))
+        
+                       return
+
+                    default :
+                }
+
+                break
+
+            
+            // Application Types
+            case FILE_TYPES.APPLICATION :
+                switch (type[1]) {
+                    case FILE_TYPES.APPLICATION_TYPES.WORD_1 :
+                    case FILE_TYPES.APPLICATION_TYPES.WORD_2 :
+                        (async () => {
+                            const { faFileWord } = await import('@fortawesome/free-solid-svg-icons')
+                            return faFileWord
+                        })().then(icon => setFileIcon(icon))
+
+                        return
+
+                    case FILE_TYPES.APPLICATION_TYPES.POWERPOINT_1 :
+                    case FILE_TYPES.APPLICATION_TYPES.POWERPOINT_2 :
+                    case FILE_TYPES.APPLICATION_TYPES.POWERPOINT_3 :
+                        (async () => {
+                            const { faFilePowerpoint } = await import('@fortawesome/free-solid-svg-icons')
+                            return faFilePowerpoint
+                        })().then(icon => setFileIcon(icon))
+
+                    return
+
+                    case FILE_TYPES.APPLICATION_TYPES.PDF :
+                        (async () => {
+                            const { faFilePdf } = await import('@fortawesome/free-solid-svg-icons')
+                            return faFilePdf
+                        })().then(icon => setFileIcon(icon))
+
+                    return
+
+                    case FILE_TYPES.APPLICATION_TYPES.EXCEL_1 :
+                    case FILE_TYPES.APPLICATION_TYPES.EXCEL_2 :
+                    case FILE_TYPES.APPLICATION_TYPES.EXCEL_3 :
+                    case FILE_TYPES.APPLICATION_TYPES.EXCEL_4 :
+                        (async () => {
+                            const { faFileExcel } = await import('@fortawesome/free-solid-svg-icons')
+                            return faFileExcel
+                        })().then(icon => setFileIcon(icon))
+
+                        return
+
+                    case FILE_TYPES.APPLICATION_TYPES.ARCHIVE_1 :
+                    case FILE_TYPES.APPLICATION_TYPES.ARCHIVE_2 :
+                    case FILE_TYPES.APPLICATION_TYPES.ARCHIVE_3 :
+                        (async () => {
+                            const { faFileArchive } = await import('@fortawesome/free-solid-svg-icons')
+                            return faFileArchive
+                        })().then(icon => setFileIcon(icon))
+
+                    return
+
+                    case FILE_TYPES.APPLICATION_TYPES.CSV :
+                        (async () => {
+                            const { faFileCsv } = await import('@fortawesome/free-solid-svg-icons')
+                            return faFileCsv
+                        })().then(icon => setFileIcon(icon))
+
+                    return
+
+                    default :
+                }
+                
+                break
+    
 
             default :
                 (async () => {
