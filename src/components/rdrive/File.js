4044649ae8dcedcb9c0fc36b983d3a62ce468bef
RichProsper <r.stp.p@hotmail.com>
2021-10-15 23:38:35 -0400

    Minor Update

--- src/components/rdrive/File.js
+++ src/components/rdrive/File.js
@@ -1,6 +1,6 @@
 import classes from './File.module.css'
 import FileIcon from './FileIcon'
-import { ITEM_TYPES } from '../hooks/useUpdateDelete'
+import { ITEM_TYPES } from './ContextMenu'
 
 export default function File({ file }) {
     const dArr = file.modifiedAt ? new Date(file.modifiedAt.toDate()).toDateString().split(' ') : null
